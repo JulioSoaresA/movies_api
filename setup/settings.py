3ce132675b91941fd91344c9993d09ca4f14c479
Júlio Soares <juliosoares@b2bit.company>
2025-01-09 10:45:04 -0300

    Feat: Add django-cors-headers for CORS support

--- setup/settings.py
+++ setup/settings.py
@@ -53,9 +53,11 @@ INSTALLED_APPS = [
     'users',
     'whitenoise.runserver_nostatic',
     'drf_yasg',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -187,3 +189,16 @@ SIMPLE_JWT = {
 
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    'movies-api-juliocsoares.fly.dev'
+]
